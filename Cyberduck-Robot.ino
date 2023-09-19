c5c55fffb1ca67d348b1563cbbd7219f1a5cf289
Violeta TM <80209320+Violeta-Tejera@users.noreply.github.com>
2023-09-19 19:12:57 +0200

    cambios de hoy con carmen (aun no funciona todo)
    el problema está en la velocidad de las ruedas, no cambia, solo sube.
    
    el error no se registra bien (va lento)
    
    los giros no los hace
    
    los sensores creemos que si  funcionan

--- Cyberduck-Robot.ino
+++ Cyberduck-Robot.ino
@@ -10,8 +10,8 @@
 #define stdSpeed 230
 
 // Sensors
-const int sensorCount = 3;
-int sensorPins[sensorCount] = {6, 7, 8}; // From left to right (travelwise)
+const int sensorCount = 5;
+int sensorPins[sensorCount] = {8, 7, 6, 12, 13}; // From left to right (travelwise)
 int sensorValues[sensorCount];
 
 //PID
@@ -22,15 +22,15 @@ int error = 0;
 int previous_error = 0;
 
 // Proportional Control
-const int kp = 0;
+const int kp = 1;
 
 // Integral control
 int integral = 0;
-const int ki = 0;
+const int ki = 1;
 
 // Derivative control
 int derivative = 0;
-const int kd = 0;
+const int kd = 1;
 
 
 
@@ -50,15 +50,18 @@ void setup(){
 }
 
 void loop(){
-  
-  /*Serial.print("Sensors left to right: ");
+  /*
+  Serial.print("Sensors left to right: ");
   for(int i = 0; i < sensorCount; i++){
     Serial.print(sensorValues[i]);
   }
   Serial.println();
-  delay(1000);*/
+  delay(1000);
+  */
 
-  forwardMovement(255, 255);
+  robot_movement();
+
+  //test_motor();
 }
 
 // @brief This method will update the array sensorValues with the current digital read of all the sensors in the array
@@ -69,45 +72,68 @@ void readSensors(){
 }
 
 void robot_movement(){
-  readSensors();
+  readSensors(); // (0 0 1 1 0)
   int sensorCum =0;
 
   // Calculate the accomulative sensors value
   for(int i = 0; i < sensorCount; i++){
-    sensorCum += sensorValues[i] * pow(2, i);
+    sensorCum += sensorValues[i] * pow(2, sensorCount-1-i);
+    
   }
 
-  
   // Get the error for the current disposition of sensors
   
   previous_error = error;
   integral += previous_error;
-  derivative = error-previous_error;
+  
   switch(sensorCum){
-    case 0b000: 
+    case 0b11111: 
       // Leftwards of the track
-      if(previous_error < 0) error = -2;
+      if(previous_error < 0) error = -3;
       // Rightwards of the track
-      else error = 2;
+      else error = 3;
      break;
     
-    case 0b100:
+    case 0b10111:
       error = -1;
       break;
-    
-    case 0b001:
+
+    case 0b11101:
       error = 1;
       break;
-      
+
+    case 0b01111:
+      error = -2;
+      break;
+
+     case 0b11110:
+      error = 2;
+      break;
+          
     // In either case we want to continue moving straight
-    case 0b010:
-    case 0b111:
+    case 0b11011:
+    case 0b00000:
       error = 0;
       break;
+
+     default:
+      // Leftwards of the track
+      if(previous_error < 0) error = -3;
+      // Rightwards of the track
+      else error = 3;
+     break;
+  }
+  
+  derivative = error-previous_error;
+
+  if(error < 0){
+    pid_left *= -1;
+  }else if(error > 0){
+    pid_right *= -1;
   }
 
   pid = pid_steering_value();
-  pid_left = stdSpeed - pid;
+  pid_left = stdSpeed + pid; //quizás lo tengo que invertir
   pid_right = stdSpeed + pid;
 
   if(pid_left < 0) pid_left = 0;
@@ -117,6 +143,27 @@ void robot_movement(){
   else if(pid_right > 255) pid_right = 255; 
 
   forwardMovement(pid_left, pid_right);
+
+  Serial.print(sensorCum);
+  Serial.print("\n");
+  Serial.print(error);
+  Serial.print("\n");
+  Serial.print(pid_left);
+  Serial.print(" ");
+  Serial.print(pid_right);
+  Serial.print("\n\n");
+  delay(1000);
+
+  /*
+  if(error < 0){
+    turnRight(pid_left, pid_right);
+  }else if(error > 0){
+    turnLeft(pid_left, pid_right);
+  }
+  else{
+    forwardMovement(pid_left, pid_right);
+  }
+  */
 }
 
 void forwardMovement(int speedLeft, int speedRight){
@@ -153,6 +200,15 @@ void turnRight(int speedLeft, int speedRight){
   digitalWrite(right_in1, HIGH);
   digitalWrite(right_in2, LOW);
   analogWrite(enableRight, speedRight);  
+}
+
+void test_motor(){
+  forwardMovement(stdSpeed, stdSpeed);
+  delay(3000);
+  turnLeft(stdSpeed, stdSpeed);
+  delay(1500);
+  turnRight(stdSpeed, stdSpeed);
+  delay(1500);
 }
 
 int pid_steering_value(){
